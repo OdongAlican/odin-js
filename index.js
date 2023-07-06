84031dc2b1a39a6e631d77ea5fc0c103238baba2
Sunday Odong <sandieo.2020@gmail.com>
2023-07-06 21:57:17 +0300

    add computer function

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('inside js')
+function getComputerChoice () {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    return computerChoice;
+}
+
+getComputerChoice();
